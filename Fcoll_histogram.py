436f144b43c6f60d0f47bdc1f2cb20d1eddb187f
binaryfunt <binaryfunt@users.noreply.github.com>
2017-03-06 21:28:48 +0000

    Improve command line argument support

--- Fcoll_histogram.py
+++ Fcoll_histogram.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 import sys, getopt
 
-USAGE = "USAGE: Fcoll_histogram.py -i <Fcoll data file>"
+USAGE = "USAGE: Fcoll_histogram.py -i <Fcoll data file> [--number=<number of bins>]"
 
 file_in = ""
+number = 50
 
 def load_binary_data(filename, dtype=np.float32):
      """
@@ -20,7 +22,7 @@ def load_binary_data(filename, dtype=np.float32):
      return _data
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "h:i:")
+    opts, args = getopt.getopt(sys.argv[1:], "h:i:n:", ["number="])
 except getopt.GetoptError:
     print USAGE
     sys.exit(2)
@@ -30,22 +32,28 @@ for opt, arg in opts:
         print USAGE
         sys.exit()
     elif opt in ("-i"):
-        print "Opening Fcoll file at", arg
         file_in = arg
-        break
-if not opts:
+    elif opt in ("-n", "--number"):
+        number = int(arg)
+if file_in == "":
     print USAGE
     sys.exit()
 
+print "Opening Fcoll file at", file_in
 Fcoll = load_binary_data(file_in)
+# Fcoll2 = 1./Fcoll
+# Fcoll3 = Fcoll2[Fcoll2 < 1e308]
+
+print "Number of bins:", number
 
 fig = plt.figure()
 ax = plt.subplot()
 
-n, bins, patches = plt.hist(Fcoll, 50)
+n, bins, patches = plt.hist(Fcoll, number)
 
 plt.xlabel("Collapse fraction")
 plt.ylabel("Frequency")
 ax.set_yscale("log")
+ax.set_xscale("log")
 
 plt.show()
